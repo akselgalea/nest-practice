5c760d462866bbdae841fd2eba7b408201187b25
Axel Galea <akselgalea@gmail.com>
2024-10-02 23:37:12 -0300

    validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req } from "@nestjs/common";
 import type { Request } from "express";
-import type { UsersService } from "./users.service";
+import { UsersService } from "./users.service";
+import { LoginDto } from "./dto/auth/login.input";
 
 @Controller("users")
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
+	@Get()
+	findAll() {
+		return this.usersService.findAll();
+	}
+
 	@Post("auth/login")
-	login(@Req() { body }: Request) {
+	login(@Body() body: LoginDto) {
 		console.log(body);
-		return this.usersService.login("a", "asdasdasd");
+		return this.usersService.login(body.email, body.password);
 	}
 }
